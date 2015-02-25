4b8fe63ef7274a8a77c3f887e42f7ab257fb11ff
polson <github.com@anotherdev.com>
2015-02-25 10:48:24 +0700

    remove Urban Airship entries that are already uninstalled or opted out.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,19 @@
+Parse.Cloud.useMasterKey();
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.job("removeUnusedUrbanAirship", function(request, status) {
+    var UA_INSTALLED = "uaInstalled";
+    var UA_OPT_IN = "uaOptIn";
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo(UA_INSTALLED, false)
+        .equalTo(UA_OPT_IN, false);
+    var count = 0;
+
+    query.each(function(install) {
+        count++;
+        install.destroy();
+    }).then(function() {
+        status.success(count + " Urban Airship entries removed");
+    }, function(error) {
+        status.error(error);
+    });
 });
