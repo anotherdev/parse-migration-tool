a77230df02e8ddff1120999b414c6eea0cc3cd9e
polson <github.com@anotherdev.com>
2015-02-27 16:39:04 +0700

    add count function

--- cloud/main.js
+++ cloud/main.js
@@ -18,3 +18,21 @@ Parse.Cloud.job("removeUnusedUrbanAirship", function(request, status) {
         status.error("Removed: " + count + " before " + error.message);
     });
 });
+
+Parse.Cloud.define("countUnusedUrbanAirship", function(request, response) {
+    var UA_INSTALLED = "uaInstalled";
+    var UA_OPT_IN = "uaOptIn";
+    var query = new Parse.Query(Parse.Installation);
+    query.equalTo(UA_INSTALLED, false)
+        .equalTo(UA_OPT_IN, false);
+
+    query.count({
+        success: function(count) {
+            console.log("Unused UA: " + count);
+            response.success();
+        },
+        error: function() {
+            response.error("Error " + error.code + " : " + error.message + " when getting count.");
+        }
+    });
+});
