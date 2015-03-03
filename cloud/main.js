d79c34765c61b86a66e97cf53c82c23bbd5fc31c
polson <github.com@anotherdev.com>
2015-03-03 16:59:48 +0700

    add common channel for all install

--- cloud/main.js
+++ cloud/main.js
@@ -36,3 +36,44 @@ Parse.Cloud.define("countUnusedUrbanAirship", function(request, response) {
         }
     });
 });
+
+Parse.Cloud.job("addAllChannel", function(request, status) {
+    var ALL_CHANNEL = "ALL";
+    var CHANNELS = "channels";
+    var query = new Parse.Query(Parse.Installation);
+    query.notContainedIn(CHANNELS, [ALL_CHANNEL]);
+    var count = 0;
+
+    query.each(function(install) {
+        count++;
+        var channels = install.get(CHANNELS);
+        // http://stackoverflow.com/a/26633883/802421
+        if (channels && channels.constructor === Array) {
+            channels.push(ALL_CHANNEL);
+        } else {
+            install.set(CHANNELS, new Array(ALL_CHANNEL));
+        }
+        install.save();
+        status.message(count.toString());
+    }).then(function() {
+        status.success(count + " ParseInstallations without \"ALL\" channel");
+    }, function(error) {
+        status.error(count + " ParseInstallations without \"ALL\" channel handled" + " before " + error.message);
+    });
+});
+
+Parse.Cloud.define("countInstallWithoutAllChannel", function(request, response) {
+    var ALL_CHANNEL = "ALL";
+    var query = new Parse.Query(Parse.Installation);
+    query.notContainedIn("channels", [ALL_CHANNEL]);
+
+    query.count({
+        success: function(count) {
+            console.log(count + " ParseInstallations without \"ALL\" channel");
+            response.success(count + " ParseInstallations without \"ALL\" channel");
+        },
+        error: function() {
+            response.error(error.message);
+        }
+    });
+});
